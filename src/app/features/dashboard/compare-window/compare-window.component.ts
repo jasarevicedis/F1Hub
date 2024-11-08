25500cab737cab6b235002bc13bc6a9d7e76bfe3
jasarevicedis <e.jasarevic03@gmail.com>
2024-11-08 13:27:12 +0100

    update dashboard with components

---
+++ src/app/features/dashboard/compare-window/compare-window.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compare-window',
+  standalone: true,
+  imports: [],
+  templateUrl: './compare-window.component.html',
+  styleUrl: './compare-window.component.scss'
+})
+export class CompareWindowComponent {
+
+}
---
+++ src/app/features/dashboard/layout-tracker/layout-tracker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-tracker',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout-tracker.component.html',
+  styleUrl: './layout-tracker.component.scss'
+})
+export class LayoutTrackerComponent {
+
+}
